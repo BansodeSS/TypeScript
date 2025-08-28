577f53c5010666942a6f5a4f88918d88296f2471
BansodeSS <er.suhasbansode@gmail.com>
2025-08-28 15:03:34 +0530

    feat: add boolean.ts file

---
+++ boolean.ts
@@ -0,0 +1,16 @@
+/**
+ * boolean.ts
+ * TypeScript Boolean Tutorial
+ */
+
+// Basic boolean declaration
+let isDone: boolean = false;
+
+// Boolean operations
+let isTrue: boolean = true;
+let isFalse: boolean = !isTrue;
+
+// Usage
+console.log("isDone:", isDone);
+console.log("isTrue:", isTrue);
+console.log("isFalse:", isFalse);
