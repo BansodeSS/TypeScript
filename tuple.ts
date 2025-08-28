66ad49021c6f05ecc2c1b264e2fc34767ebda8bb
BansodeSS <er.suhasbansode@gmail.com>
2025-08-28 15:10:51 +0530

    Add tuple.ts file with initial implementation

---
+++ tuple.ts
@@ -0,0 +1,16 @@
+/**
+ * tuple.ts
+ * TypeScript Tuple Tutorial
+ */
+
+// Basic tuple declaration
+let user: [string, number] = ["Suhas", 30];
+
+// Accessing tuple elements
+let tupleUserName: string = user[0];
+let userAge: number = user[1];
+
+// Usage
+console.log("user:", user);
+console.log("userName:", tupleUserName);
+console.log("userAge:", userAge);
